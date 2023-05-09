f8a7d013b622353e8ebc18eb1048cd87bd7a2547
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-05-09 23:14:53 +0530

    Add files via upload

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+ import { Navigate } from "react-router-dom";
+ 
+ 
+ 
+
+const PrivateRoute = () =>{
+    
+ return (<Navigate to="/"/>);
+};
+
+export default PrivateRoute;
